548a05fa52343979ac4487cd0280016b7b020552
Juan Pablo Contreras Pardo <juanpaconpa@192.168.1.2>
2023-04-13 20:53:56 -0500

    feat(Pipfile docker-compose.yml postgresql_connect.py): postgres image dockerized and configured

---
+++ src/app/infrastructure/db/postgresql_connect.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+load_dotenv()
+
+engine = create_engine(
+    os.getenv('DATABASE_URL'), connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
